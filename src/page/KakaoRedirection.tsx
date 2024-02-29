4f4769701cd3a0a0b1d10c25a091fcd531381535
CosmicLatte009 <ohhm009@gmail.com>
2024-02-29 14:34:18 +0900

    (#7) feat: 백엔드에 인가코드 보낸 후 token GET해오기

--- src/page/KakaoRedirection.tsx
+++ src/page/KakaoRedirection.tsx
@@ -1,21 +1,26 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { useEffect } from "react";
 import axios from "axios";
 import BASE_URL from "../utils/BASE_URL";
 
 export const KakaoRedirection = () => {
-  const code = window.location.search;
+  const [searchParams] = useSearchParams();
+  const code = searchParams.get("code");
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log(BASE_URL);
-    axios.get(`${BASE_URL}/kakao/callback?${code}`).then(response => {
-      console.log(response.data);
+    axios.get(`${BASE_URL}/kakao/callback?code=${code}`).then(response => {
+      console.log(response.data.token);
 
       localStorage.setItem("name", response.data.user_name);
-      localStorage.setItem("token", response.data.access_token);
+      localStorage.setItem("token", response.data.token);
 
-      navigate("/");
+      const token = localStorage.getItem("token");
+      if (token !== undefined) {
+        navigate("/");
+      } else {
+        console.log("token 없음");
+      }
     });
   }, []);
 
