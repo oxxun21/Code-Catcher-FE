e316833ee397af9118ded065f117c988eac72334
Oh Ga Eun <dhrkdms4113@naver.com>
2024-03-04 11:25:26 +0900

    (#17) add: 404 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Global } from "@emotion/react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import reset from "./style/reset";
 import { Home } from "./page/Home";
 import { Splash } from "./page/Splash";
 import { KakaoRedirection } from "./page/KakaoRedirection";
 import { CodingTest } from "./page/CodingTest";
 import { CodeCompare } from "./page/CodeCompare";
+import { NotFound } from "./page/NotFound";
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         <Route path="/kakao/callback" element={<KakaoRedirection />} />
         <Route path="/CodingTest" element={<CodingTest />} />
         <Route path="/CodeCompare" element={<CodeCompare />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path={"*"} element={<Navigate to="/404" />} />
       </Routes>
     </BrowserRouter>
   );
