ecd8dfc78ef284cc10cc35164a3f2a62bcf4f98e
CosmicLatte009 <ohhm009@gmail.com>
2024-03-05 12:02:05 +0900

    (#7) refactor: axios instance로 카카오 인가코드 api 요청 로직 변경 및 후속 처리에 setLoginCookie, setUserInfo 사용

--- src/page/KakaoRedirection.tsx
+++ src/page/KakaoRedirection.tsx
@@ -1,27 +1,33 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useEffect } from "react";
-import axios from "axios";
-import BASE_URL from "../utils/BASE_URL";
+import { instance } from "../api/instance";
+import { setLoginCookie } from "../utils/loginCookie";
+import { useUserStore } from "../stores/useUserStore";
 
 export const KakaoRedirection = () => {
   const [searchParams] = useSearchParams();
   const code = searchParams.get("code");
   const navigate = useNavigate();
+  const setUserInfo = useUserStore(state => state.setUserInfo);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/kakao/callback?code=${code}`).then(response => {
-      console.log(response.data.token);
-
-      localStorage.setItem("name", response.data.user_name);
-      localStorage.setItem("token", response.data.token);
-
-      const token = localStorage.getItem("token");
-      if (token !== undefined) {
-        navigate("/");
-      } else {
-        console.log("token 없음");
-      }
-    });
+    instance
+      .get(`kakao/callback?code=${code}`)
+      .then(response => {
+        const resData = response.data;
+        console.log(resData);
+        const { jwt, userId, nickname } = response.data;
+        if (resData) {
+          setLoginCookie(jwt, { path: "/" });
+          setUserInfo(userId, nickname);
+          navigate("/");
+        } else {
+          console.log("token 없음");
+        }
+      })
+      .catch((err: Error) => {
+        console.error("로그인 오류 발생", err);
+      });
   }, []);
 
   return <div>로그인 중입니다.</div>;
