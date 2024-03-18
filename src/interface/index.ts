876f156090438d60fb7bdeffbe67eb19f219f054
CosmicLatte009 <ohhm009@gmail.com>
2024-03-18 16:43:59 +0900

    (#55) add: mypageInfo interface 작성

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -4,4 +4,4 @@ export * from "./questionOutline_I";
 export * from "./testScoreSubmit_I";
 export * from "./scoreSubmit_I";
 export * from "./submissionProps_I";
-export * from "./mypageListItem_I";
+export * from "./mypageInfo_I";
