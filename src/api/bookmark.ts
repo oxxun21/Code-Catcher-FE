8b662dc6042a1d4bee9b383490fe86957890d6e1
Oh Ga Eun <dhrkdms4113@naver.com>
2024-03-18 12:33:31 +0900

    (#16) add: 북마크 API 추가

---
+++ src/api/bookmark.ts
@@ -0,0 +1,27 @@
+import { Bookmark_I, ScoreSubmit_I, SubmissionProps_I } from "../interface";
+import { instance } from "./instance";
+
+export const postBookmarkAPI = async ({ problemId, codeType, code }: SubmissionProps_I): Promise<ScoreSubmit_I> => {
+  try {
+    const response = await instance.post("/bookmark", {
+      problemId,
+      codeType,
+      code,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getBookmarkAPI = async (id: string | undefined): Promise<Bookmark_I> => {
+  if (id === undefined) {
+    throw new Error("ID is undefined");
+  }
+  try {
+    const response = await instance.get(`/bookmark?id=${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
