c8c703de555730dc46e11b250e388f4f081efa62
CosmicLatte009 <ohhm009@gmail.com>
2024-03-21 02:15:31 +0900

    (#69) design: 캐러셀 배경색상 및 이미지 변경

---
+++ src/assets/carousel/index.ts
@@ -0,0 +1,6 @@
+import carouselImg1 from "./carousel_01.png";
+import carouselImg2 from "./carousel_02.png";
+import carouselImg3 from "./carousel_03.png";
+import carouselImg4 from "./carousel_04.png";
+
+export { carouselImg1, carouselImg2, carouselImg3, carouselImg4 };
