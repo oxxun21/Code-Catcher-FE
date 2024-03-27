07bc596ec1c959892caf8ce903aa04cbfc8d90fb
Oh Ga Eun <ogaeun0121@gmail.com>
2024-03-27 19:58:19 +0900

    fix Gutter design

--- src/components/common/Gutter.tsx
+++ src/components/common/Gutter.tsx
@@ -15,16 +15,21 @@ export const Gutter = ({ orientation, onMouseDown, changeBackColor }: GutterProp
 const GutterStyle = styled.div<GutterProps>`
   width: ${props => props.orientation === "horizontal" && "24px"};
   height: ${props => props.orientation === "vertical" && "24px"};
-  background: ${props =>
+  /* background: ${props =>
     props.changeBackColor && props.orientation === "horizontal"
-      ? `#3F3F47 url(${gutter_horizontal}) no-repeat center`
+      ? `#020202 url(${gutter_horizontal}) no-repeat center`
       : props.changeBackColor && props.orientation !== "horizontal"
       ? `#3F3F47 url(${gutter_vertical}) no-repeat center`
       : props.orientation === "horizontal"
       ? `url(${gutter_horizontal}) no-repeat center`
       : `url(${gutter_vertical}) no-repeat center`};
+  background-size: ${props => (props.orientation === "horizontal" ? "auto/40px" : "40px/auto")}; */
+  background: ${props =>
+    (props.changeBackColor ? (props.orientation === "horizontal" ? "#020202" : "#3F3F47") : "") +
+    ` url(${props.orientation === "horizontal" ? gutter_horizontal : gutter_vertical})` +
+    " no-repeat center /" +
+    (props.orientation === "horizontal" ? "auto 40px" : "40px auto")};
   position: relative;
-  background-size: ${props => (props.orientation === "horizontal" ? "auto/40px" : "40px/auto")};
   border-right: ${props => props.orientation === "horizontal" && "2px solid var(--background-color)"};
   border-top: ${props => props.orientation === "vertical" && "2px solid var(--background-color)"};
   cursor: ${props => (props.orientation === "horizontal" ? "e-resize" : "n-resize")};
