f868adea4b7d7ef77eeaeddd98df0a978c40131a
Oh Ga Eun <ogaeun0121@gmail.com>
2024-03-27 18:46:19 +0900

    fix: 탈퇴 후 splash 이동

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -52,6 +52,7 @@ export const Header = () => {
       if (status === 200) {
         alert("회원 탈퇴가 성공적으로 처리되었습니다.");
       }
+      navigate("/splash");
     } catch (error) {
       console.error("회원 탈퇴 처리 중 오류가 발생했습니다.", error);
     }
