d4ea8a1b25d78c799853a3162779d01e63f6f5f7
Oh Ga Eun <ogaeun0121@gmail.com>
2024-04-14 20:37:13 +0900

    meta 태그 관련 테스트

--- src/components/common/HelmetMetaTags.tsx
+++ src/components/common/HelmetMetaTags.tsx
@@ -27,7 +27,7 @@ export const HelmetMetaTags = ({ meta }: HelmetMetaTagsProps) => {
 
       <meta property="og:title" content={meta.title} />
       <meta property="og:description" content={meta.description} />
-      <meta property="og:image" content={meta.image} />
+      <meta property="og:image" content="https://likelion-codee.com/assets/meta/metaCover.png" />
       <meta property="og:url" content={meta.url} />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="코디(Codee)" />
@@ -35,7 +35,7 @@ export const HelmetMetaTags = ({ meta }: HelmetMetaTagsProps) => {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={meta.title} />
       <meta name="twitter:description" content={meta.description} />
-      <meta name="twitter:image" content={meta.image} />
+      <meta name="twitter:image" content="https://likelion-codee.com/assets/meta/metaCover.png" />
     </Helmet>
   );
 };
