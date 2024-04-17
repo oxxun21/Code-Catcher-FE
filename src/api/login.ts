23ecb0f6163e8e1c0e571ee2b28c64b2724ddc9f
CosmicLatte009 <ohhm009@gmail.com>
2024-04-18 02:18:39 +0900

    feat: GA 신규 유저와 로그인 유저 구분

--- src/api/login.ts
+++ src/api/login.ts
@@ -2,17 +2,34 @@ import { NavigateFunction } from "react-router-dom";
 import { instance } from "./instance.ts";
 import { UserState } from "../interface";
 import { setLoginCookie } from "../utils/loginCookie.ts";
+import { useEventTracker } from "../hook/useEventTracker.tsx";
 
 export const getLoginAPI = async (code: string, navigate: NavigateFunction, setUserInfo: UserState["setUserInfo"]) => {
+  const trackEvent = useEventTracker();
+
   try {
     const response = await instance.get(`kakao/callback?code=${code}`);
     const resData = response.data;
     if (resData) {
-      const { jwt, ...userInfo } = resData;
+      const { jwt, isNew, ...userInfo } = resData;
+      console.log(resData);
       setLoginCookie(jwt, { path: "/" });
       setUserInfo(userInfo);
 
       navigate("/");
+      if (isNew) {
+        trackEvent({
+          category: "Auth",
+          action: "signup success",
+          label: "Kakao",
+        });
+      } else {
+        trackEvent({
+          category: "Auth",
+          action: "login success",
+          label: "kakao",
+        });
+      }
     } else {
       console.log("token 없음");
     }
