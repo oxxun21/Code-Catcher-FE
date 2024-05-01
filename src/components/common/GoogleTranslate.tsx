dcc9507feb7bbc36cae8f13e5e29fafe24971cac
CosmicLatte009 <ohhm009@gmail.com>
2024-05-01 15:50:10 +0900

    fix: 구글번역 위젯 쿠키 조작 제거

--- src/components/common/GoogleTranslate.tsx
+++ src/components/common/GoogleTranslate.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useRef, useState, memo } from "react";
 import { useLocation } from "react-router-dom";
+import { useCookies } from "react-cookie";
 import FlagEn from "../../assets/flag_en.svg";
 import FlagKo from "../../assets/flag_ko.svg";
 import { useWindowSize } from "../../hook";
-import { useCookies } from "react-cookie";
-import { Loading } from "./Loading";
 
 declare global {
   interface Window {
@@ -18,41 +17,12 @@ export const GoogleTranslate = memo(() => {
   const { width } = useWindowSize();
   const isMobile = (width ?? 0) <= 480;
   const [cookies] = useCookies(["googtrans"]);
-  const [loading, setLoading] = useState(false);
+  const [translateWidgetLoaded, setTranslateWidgetLoaded] = useState(false);
   const [isGoogTransKo, setIsGoogTransKo] = useState<boolean>(true);
   const location = useLocation();
 
-  const removeGoogleTransCookies = () => {
-    const paths = [
-      "/",
-      "/bookmark/",
-      "/myPage",
-      "/bookmarkList",
-      "/lastQuestionList",
-      "/codingTest/",
-      "/codeCompare/",
-      "/splash",
-      "/question/select",
-    ];
-    paths.forEach(path => {
-      document.cookie = "googtrans=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=" + path + ";";
-    });
-
-    window.location.reload();
-  };
-
   useEffect(() => {
-    loadGoogleTranslateScript();
-    return () => unloadGoogleTranslateScript();
-  }, [location]);
-
-  const loadGoogleTranslateScript = () => {
-    setLoading(true);
     const scriptId = "google-translate-script";
-    if (document.getElementById(scriptId)) {
-      setLoading(false);
-      return;
-    }
     const script = document.createElement("script");
     script.id = scriptId;
     script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
@@ -61,52 +31,47 @@ export const GoogleTranslate = memo(() => {
     window.googleTranslateElementInit = () => {
       if (translateElementRef.current) {
         new window.google.translate.TranslateElement(
-          {
-            pageLanguage: "ko",
-            autoDisplay: true,
-          },
-          translateElementRef.current.id
+          { pageLanguage: "ko", autoDisplay: true },
+          translateElementRef.current?.id
         );
       }
+      setTranslateWidgetLoaded(true);
     };
 
-    script.onload = () => {
-      setIsGoogTransKo(!(cookies.googtrans && cookies.googtrans === "/ko/en"));
-      setLoading(false);
-    };
-  };
+    script.onload = () => setTranslateWidgetLoaded(true);
+    script.onerror = () => setTranslateWidgetLoaded(false);
 
-  const unloadGoogleTranslateScript = () => {
-    const script = document.getElementById("google-translate-script");
-    if (script) {
+    return () => {
       document.body.removeChild(script);
+      delete window.googleTranslateElementInit;
+    };
+  }, [location]);
+
+  useEffect(() => {
+    if (translateWidgetLoaded) {
+      const currentLang = cookies.googtrans === "/ko/en" ? "en" : "ko";
+      refreshTranslateElement(currentLang);
     }
-    delete window.googleTranslateElementInit;
-  };
+  }, [translateWidgetLoaded, cookies.googtrans]);
 
   const refreshTranslateElement = (langCode: string) => {
-    if (langCode === "en") {
-      const gtcombo = document.querySelector(".goog-te-combo") as HTMLSelectElement;
-      if (gtcombo) {
-        gtcombo.value = langCode;
-        gtcombo.dispatchEvent(new Event("change"));
-        setIsGoogTransKo(false);
-      } else {
-        if (!loading) {
-          setTimeout(() => refreshTranslateElement(langCode), 500);
-        }
-      }
-    } else if (langCode === "ko") {
-      unloadGoogleTranslateScript();
-      removeGoogleTransCookies();
-      window.location.reload();
-      setIsGoogTransKo(true);
+    console.log("[langCode]", langCode);
+    const gtcombo = document.querySelector(".goog-te-combo") as HTMLSelectElement;
+    if (!gtcombo) {
+      console.log("Waiting for gtcombo to be available...");
+      setTimeout(() => refreshTranslateElement(langCode), 500);
+      return;
     }
+
+    gtcombo.value = langCode; // 변경할 언어 적용
+    gtcombo.dispatchEvent(new Event("change")); // 변경 이벤트 트리거
+    setIsGoogTransKo(langCode === "ko");
+
+    console.log("[gtCombo]", gtcombo.value);
   };
 
   return (
     <div>
-      {loading && <Loading />}
       <div ref={translateElementRef} id="google_translate_element" style={{ display: "none" }}></div>
       <ul className="translation-links">
         {isGoogTransKo ? (
